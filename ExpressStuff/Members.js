9a14ff6068d8d3e9703b9dc5d09d8be6dd6abf36
ehv <ehv@iastate.edu>
2019-09-09 19:45:37 -0500

    Followed Traversy Media Node and Express 2019 Crash Course

---
+++ ExpressStuff/Members.js
@@ -0,0 +1,21 @@
+const members = [
+    {
+        id: 1,
+        name: 'Ethan Vander Wiel',
+        email: 'ehv@iastate.edu',
+        status: 'active'
+    },
+    {
+        id: 2,
+        name: 'Willis Knox',
+        email: 'wpknox@iastate.edu',
+        status: 'active'
+    },
+    {
+        id: 3,
+        name: 'Brett Santema',
+        email: 'bsantema@iastate.edu',
+        status: 'inactive'
+    }
+]
+module.exports = members;
